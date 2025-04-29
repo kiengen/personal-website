2fbe2532a5a0921718d42957c56f725c868764de
Kiersten E <152218847+kiengen@users.noreply.github.com>
2025-04-29 13:28:23 -0400

    website template, rudimentary header

---
+++ personal-website/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import HelloWorld from './components/HelloWorld.vue';
+    import Header from './components/Header.vue';
+</script>
+
+<template>
+	<Header />
+    <div>
+        <a href="https://vite.dev" target="_blank">
+            <img src="/vite.svg" class="bg-white" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+</template>
---
+++ personal-website/src/components/HeaderButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    type buttonFunc = () => void;
+
+    defineProps<{ msg: string
+                  action: buttonFunc
+                }>()
+    const isDarkTheme = ref(false);
+</script>
+
+<template>
+    <h1 class="" @click="console.log(typeof(action))">{{msg}}</h1>
+</template>
